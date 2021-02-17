382d8b4ee2e05c07f309109e8674b0c0e8843f73
Lbniese <lbniese@gmail.com>
2021-02-17 20:30:10 +0100

    initial clockface work
    *Added inner ring and clock enter

--- Clock/scripts.js
+++ Clock/scripts.js
@@ -1,4 +1,4 @@
-console.log("Loaded: scripts.js")
+console.log("Loaded: scripts.js");
 
 // // retrieve the node in the DOM representing the <canvas> element
 const canvas = document.getElementById("canvas");
@@ -15,10 +15,38 @@ radius = radius * 0.90
 drawClock();
 
 
-// method to draw clock - currently draws white circle
+// drawClock() calls the functions that draws each part of the clock
 function drawClock() {
-    console.log("Called: drawClock()")
+    console.log("Called: drawClock()");
+    drawFace(ctx, radius);
+}
+
+function drawFace(ctx, radius) {
+    console.log("Called: drawFace()");
+
+    let grad;
+
+    // draw white circle
+    ctx.beginPath();
     ctx.arc(0, 0, radius, 0, 2 * Math.PI);
     ctx.fillStyle = "white";
+    ctx.fill();
+
+    // draw pastelle green ring
+    // radial gradient (95% and 105% of original clock radius)
+    grad = ctx.createRadialGradient(0, 0, radius * 0.95, 0, 0, radius * 1.05);
+    // adds pastelle green color to the ring
+    grad.addColorStop(0, '#CAE7C1');
+    // define the gradient as the stroke style of the drawing object
+    ctx.strokeStyle = grad;
+    // define the line width of the drawing object (10% of radius)
+    ctx.lineWidth = radius * 0.1;
+    // draw circle
+    ctx.stroke();
+
+    // draw clock center
+    ctx.beginPath();
+    ctx.arc(0, 0, radius * 0.1, 0, 2 * Math.PI);
+    ctx.fillStyle = '#CAE7C1';
     ctx.fill();
 }
